c9a3818df62d764cab06022a2682d26e9fddb3ff
gmm <miaomiaogong92@gmail.com>
2017-12-29 23:14:09 +0800

    完成

--- latte-compiler/src/main/java/com/gmm/latte/compiler/PayEntryVisitor.java
+++ latte-compiler/src/main/java/com/gmm/latte/compiler/PayEntryVisitor.java
@@ -50,9 +50,6 @@ public final class PayEntryVisitor extends SimpleAnnotationValueVisitor7<Void, V
                 .addFileComment("微信支付入口文件")
                 .build();
         try {
-
-
-
             javaFile.writeTo(mFiler);
         } catch (IOException e) {
             e.printStackTrace();
