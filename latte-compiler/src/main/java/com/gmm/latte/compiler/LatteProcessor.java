7117744300a5ae563391ee38de00b58221eca57e
gmm <miaomiaogong92@gmail.com>
2018-01-26 19:01:19 +0800

    搭建首页,完成数据解析框架

--- latte-compiler/src/main/java/com/gmm/latte/compiler/LatteProcessor.java
+++ latte-compiler/src/main/java/com/gmm/latte/compiler/LatteProcessor.java
@@ -80,6 +80,9 @@ public class LatteProcessor extends AbstractProcessor {
         scan(environment, PayEntryGenerator.class, visitor);
     }
 
+
+
+
     private void generateAppRegisterCode(RoundEnvironment environment) {
         final AppRegisterVisitor visitor = new AppRegisterVisitor();
         visitor.setFiler(processingEnv.getFiler());
